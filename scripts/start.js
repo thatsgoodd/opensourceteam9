6c893334e1baf381cef6852b9bdb6b821e6f942f
20221157 <20221157@sungshin.ac.kr>
2024-12-07 14:13:49 +0900

    Set up BrowserSync to watch CSS files in dist directory

--- scripts/start.js
+++ scripts/start.js
@@ -3,7 +3,7 @@ const upath = require('upath');
 
 const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
 
-concurrently([
+/*concurrently([
     { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
     { 
         command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
@@ -14,7 +14,17 @@ concurrently([
     prefix: 'name',
     killOthers: ['failure', 'success'],
 }).then(success, failure);
-
+*/
+concurrently([
+	{
+		command: `"${browserSyncPath}" dist -w --files dist/**/*.css --no-online`,
+		name: 'SB_BROWSER_SYNC',
+		prefixColor: 'bgGreen.bold',
+	}
+], {
+	prefix: 'name',
+	killOthers: ['failure', 'success'],
+})
 function success() {
     console.log('Success');    
 }
